56ae2ff11cd5670d43a679ea7743027491717bec
nareshkhanduri <nareshkhanduri@yahoo.com>
2015-04-20 21:23:48 +0530

    New Layout

--- app/src/main/java/gulak/chittyalpha/MainActivity.java
+++ app/src/main/java/gulak/chittyalpha/MainActivity.java
@@ -16,23 +16,66 @@ import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.util.Log;
 import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.app.AlertDialog;
+import android.view.View.OnClickListener;
+import android.content.DialogInterface;
 
 
 public class MainActivity extends ActionBarActivity {
 
+private ImageView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(ImageView)findViewById(R.id.transferImg);
+        tv.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                showInputDialog();
+            }
+        });
         findViewById(R.id.imageView).setOnTouchListener(new MyTouchOne());
         findViewById(R.id.imageButton2).setOnTouchListener(new MyTouchFive());
         findViewById(R.id.imageView2).setOnTouchListener(new MyTouchTwo());
-        findViewById(R.id.editText).setOnDragListener(new MyDragListener());
+        findViewById(R.id.transferImg).setOnDragListener(new MyDragListener());
     }
 
+    protected void showInputDialog(){
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.recipentdetails, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+        TextView chittyText = (TextView)findViewById(R.id.textview);
+        alertDialogBuilder.setTitle("Chitty Points : " + chittyText.getText());
+
+        //final EditText editText = (EditText) promptView.findViewById(R.id.editText2);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //editText.setText("Hello");
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
     private final class MyTouchFive implements OnTouchListener {
         public boolean onTouch(View view, MotionEvent motionEvent) {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
@@ -95,14 +138,15 @@ public class MainActivity extends ActionBarActivity {
                 case DragEvent.ACTION_DROP:
                     // Dropped, reassign View to ViewGroup
                     ClipData clipData=event.getClipData();
-                    Log.v("MyLog","This is Coming from my logger : " + clipData.getItemAt(0));
-                    Log.v("MyLog",String.valueOf(clipData.getItemCount()));
-                    EditText myValueText = (EditText)v;
+                    TextView myValueText = (TextView)findViewById(R.id.textview);
                     String value=myValueText.getText().toString();
                     int val=new Integer(value).intValue();
                     String newVal=clipData.getItemAt(0).getText().toString();
-                    int finalVal=val+new Integer(newVal).intValue();
-                    Toast.makeText(getApplicationContext(), "Got : " + finalVal, Toast.LENGTH_SHORT).show();
+                    int finalVal=val + new Integer(newVal).intValue();
+                    if(finalVal>=10) {
+                        finalVal=val;
+                        Toast.makeText(getApplicationContext(), "Max Chitty allowd 9" , Toast.LENGTH_SHORT).show();
+                    }
                     String currentVal=String.valueOf(finalVal);
                     myValueText.setText(currentVal);
                     //int newVal=Integer.parseInt(String.valueOf(myValueText.getText()));
